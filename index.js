ee20d3744d1ce1d65629cf5ee53d0582432c15ea
Marc Bachmann <marc.brookman@gmail.com>
2022-02-03 17:50:19 +0100

    feat: Upgrade to semantic-release v19
    BREAKING CHANGE: Require semantic-release >16.0.0 with the new plugin config.
    Require at least node 14. https://github.com/semantic-release/semantic-release/releases/tag/v16.0.0

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const envCi = require('env-ci')
 const SRError = require('@semantic-release/error')
 
-module.exports = (options, config) => {
-  const {branch} = envCi()
-  const type = (config && config.nextRelease && config.nextRelease.type) || ''
+module.exports = {
+  verifyRelease (options, context) {
+    const {branch} = envCi()
+    const type = context?.nextRelease?.type || ''
 
-  const regex = new RegExp(options.applyRegex, 'g')
-  if (!regex.test(branch)) return true
+    const regex = new RegExp(options.applyRegex, 'g')
+    if (!regex.test(branch)) return true
 
-  // Only allow a patch or initial release
-  if (type === 'patch' || type === 'initial') {
-    return true
+    // Only allow a patch or initial release
+    if (type === 'patch' || type === 'initial') {
+      return true
+    }
+
+    throw new SRError(
+      `a 'minor' or a 'major' release is not allowed in a maintenance branch.\n` +
+      `Please update your commit messages in the maintenance branch.`
+    )
   }
-
-  throw new SRError(
-    `a 'minor' or a 'major' release is not allowed in a maintenance branch.\n` +
-    `Please update your commit messages in the maintenance branch.`
-  )
 }
